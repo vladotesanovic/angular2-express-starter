8f5ba3bcc980fdc0cc8036b5f8e8ba558ae68582
Vlado Tesanovic <vlado.tesanovic@gmail.com>
2016-01-17 22:21:44 +0100

    ng-semantic is included into this package

--- src/components/home.ts
+++ src/components/home.ts
@@ -1,10 +1,18 @@
 import { Component, View } from "angular2/core";
+import { SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES } from "ng-semantic/semantic";
 
 @Component({
 	selector: "home"
 })
 @View({
-	directives: [],
-	template: `<h1>Hello! <a href="#/item">Item view</a>!</h1>`
+	directives: [SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES],
+	template: `
+	<div class="ui container">
+		<sm-segment class="raised"><h1>Hello! <a href="#/item">Item view</a>!</h1></sm-segment>
+		<br/>
+		<button class="ui button green" [sm-dir-modal]="{selector: 'my-modal'}">Click me</button>
+		<sm-modal selector="my-modal" title="Hello from Modal" class="basic">Works!!!</sm-modal>
+	</div>
+	`
 })
 export class HomeComponent {}
--- src/components/item.ts
+++ src/components/item.ts
@@ -1,11 +1,18 @@
 import { Component, View } from "angular2/core";
+import { SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES } from "ng-semantic/semantic";
 
 @Component({
 	selector : "item"
 })
 @View({
-	directives: [],
-	template: `<h1>Item view! <a href="#/">Home view</a>!</h1>`
+	directives: [SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES],
+	template: `<h1>Item view! <a href="#/">Home view</a>!</h1>
+	<div class="ui container">
+		<sm-tabs>
+			<sm-tab title="Hello" class="ui tab bottom attached segment active" data-tab="first">hello...</sm-tab>
+			<sm-tab title="About" class="ui tab bottom attached segment" data-tab="second">about..</sm-tab>
+		</sm-tabs>
+	</div>`
 })
 
 export class ItemComponent {}
