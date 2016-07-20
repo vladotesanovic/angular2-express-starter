f576b3097d4e9e55a8a83c747cb410f12b499b5c
Vlado Tesanovic <vlado.tesanovic@gmail.com>
2016-07-20 23:46:53 +0200

    - Cleaning and updating

--- client/main.ts
+++ client/main.ts
@@ -2,7 +2,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { Type, enableProdMode } from "@angular/core";
 import { HTTP_PROVIDERS } from "@angular/http";
-import { GOOGLE_MAPS_PROVIDERS } from 'angular2-google-maps/core';
 
 enableProdMode();
 
@@ -11,6 +10,5 @@ import { APP_ROUTER_PROVIDERS } from "./routes";
 
 bootstrap(<Type>AppComponent, [
 	APP_ROUTER_PROVIDERS,
-	HTTP_PROVIDERS,
-	GOOGLE_MAPS_PROVIDERS
+	HTTP_PROVIDERS
 ]);
--- public/assets/js/systemjs.config.js
+++ public/assets/js/systemjs.config.js
@@ -12,16 +12,14 @@ var isPublic = typeof window != "undefined";
         '@angular/router':            (isPublic)? '@angular/router' : 'node_modules/@angular/router',
         'angular2-in-memory-web-api': (isPublic)? 'angular2-in-memory-web-api' : 'node_modules/angular2-in-memory-web-api',
         'rxjs':                       (isPublic)? 'rxjs' : 'node_modules/rxjs',
-        'ng-semantic':                (isPublic)? 'ng-semantic' : 'node_modules/ng-semantic',
-        'angular2-google-maps':                (isPublic)? 'angular2-google-maps' : 'node_modules/angular2-google-maps'
+        'ng-semantic':                (isPublic)? 'ng-semantic' : 'node_modules/ng-semantic'
     };
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
         'app':                        { main: 'main.js',  defaultExtension: 'js' },
         'rxjs':                       { defaultExtension: 'js' },
-        'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
-        'ng-semantic':                { main: 'ng-semantic', defaultExtension: 'js' },
-        'angular2-google-maps/core': { defaultExtension: 'js', main: 'index.js' }
+        'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' }, 
+        'ng-semantic':                { main: 'ng-semantic', defaultExtension: 'js' }
     };
     var ngPackageNames = [
         'common',
