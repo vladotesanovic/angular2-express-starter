8ff06a87ae51dfd5d36a959ced91d7003165a7e9
Vlado Tesanovic <vlado.tesanovic@gmail.com>
2016-07-20 23:24:18 +0200

    - Dependencies update

--- client/components/home/home.component.ts
+++ client/components/home/home.component.ts
@@ -2,17 +2,14 @@ import { Component } from "@angular/core";
 import { SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES } from "ng-semantic";
 import { Http, Headers, RequestOptions } from "@angular/http";
 import 'rxjs/add/operator/map';
-import { GOOGLE_MAPS_DIRECTIVES } from 'angular2-google-maps/core';
 
 @Component({
-	directives: [SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES, GOOGLE_MAPS_DIRECTIVES],
+	directives: [SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES],
 	selector: "home",
 	templateUrl: `client/components/home/home.component.html`
 })
 export class HomeComponent {
 	response: any;
-	lat: number = 51.678418;
-	lng: number = 7.809007;
 	data: any;
 	error: any;
 	user: any = {
