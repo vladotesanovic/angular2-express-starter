c7f3f8c642f2371898a2ac49ddff615699d19cf0
Vlado Tesanovic <vlado.tesanovic@gmail.com>
2016-08-14 17:58:19 +0200

    Fixed bundling

--- client/app.module.ts
+++ client/app.module.ts
@@ -1,13 +1,12 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from "@angular/http";
 import { SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES } from "ng-semantic"
 
 import { AppComponent }  from './components/app.component';
 import { routing } from "./routes";
-import { HttpModule } from "@angular/http";
 import { HomeComponent } from "./components/home/home.component";
 import { ContactComponent } from "./components/contact/contact.component";
-
 import { HelloComponent } from "./components/shared/hello.component";
 
 @NgModule({
@@ -16,7 +15,7 @@ import { HelloComponent } from "./components/shared/hello.component";
         HttpModule,
         routing
     ],
-    declarations: [ AppComponent, HomeComponent, ContactComponent, SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES, HelloComponent ],
+    declarations: [ HelloComponent, SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES, AppComponent, HomeComponent, ContactComponent ],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
