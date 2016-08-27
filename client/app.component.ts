45fada3d80315c5ed6287268874063b5c51b4363
Vlado Tešanović <vlado.tesanovic@gmail.com>
2016-08-28 00:07:40 +0200

    Forever package implementation

--- client/app.component.ts
+++ client/app.component.ts
@@ -2,21 +2,20 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "app",
-    template: `<div class="ui container">
-        <div class="center">
-            <img src='https://angular.io/resources/images/logos/standard/shield-large.png'>
-        </div>
-        <hello [name]="appName"></hello>
-        
-        <div class="ui divider"></div>
-        
-        <router-outlet></router-outlet>
-        
-        <nav class="ui menu inverted teal">
+    template: `<nav class="ui menu inverted teal">
             <a routerLink="home/list" class="item">Home</a>
             <a routerLink="contact" class="item">Contact Me</a>
         </nav>
-      </div>`
+<div class="ui container">
+    <div class="center">
+        <img src='https://angular.io/resources/images/logos/standard/shield-large.png'>
+    </div>
+    <hello [name]="appName"></hello>
+    
+    <div class="ui divider"></div>
+    
+    <router-outlet></router-outlet>
+</div>`
 })
 export class AppComponent {
     appName: string = "Angular 2 Express"
