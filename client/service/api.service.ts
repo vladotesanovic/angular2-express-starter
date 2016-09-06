fb80937daaded3745507737fcd29530119d6a0e8
Vlado Tešanović <vlado.tesanovic@gmail.com>
2016-09-06 12:11:03 +0200

    added map from rxjs

--- client/service/api.service.ts
+++ client/service/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { AuthHttp } from "angular2-jwt";
 import { Response } from "@angular/http";
+import "rxjs/add/operator/map";
 
 @Injectable()
 export class ApiService {
