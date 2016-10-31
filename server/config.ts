7d18cc3fd564d3b3797d5e10a0a4f90d9b998e65
Vlado Tešanović <vlado.tesanovic@gmail.com>
2016-10-31 21:03:29 +0100

    updating to correct port number

--- server/config.ts
+++ server/config.ts
@@ -1,4 +1,4 @@
-export const serverPort = 3000;
+export const serverPort = 4300;
 export const secret = 'RbBQqA6uF#msRF8s7h*?@=95HUm&DgMDd6zLFn4XzWQ6dtwXSJwBX#?gL2JWf!';
 export const length = 128;
 export const digest = 'sha256';
