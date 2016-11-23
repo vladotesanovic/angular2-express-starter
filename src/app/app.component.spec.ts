631da834061a1387c4623023bfba8211d259c3c1
Artem Zinoviev <arzinoviev@gmail.com>
2016-11-23 17:40:25 +0300

    make ng test green

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,40 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpModule } from '@angular/http';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Observable } from 'rxjs';
 import { AppComponent } from './app.component';
+
+let component:  AppComponent;
+let fixture:    ComponentFixture<AppComponent>;
 
 describe('App: Tmp', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    });
+      imports:        [HttpModule, RouterTestingModule],
+      declarations:   [AppComponent],
+      providers:      [],
+
+      schemas:        [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+
+    fixture     = TestBed.createComponent(AppComponent);
+    component   = fixture.debugElement.componentInstance;
   });
 
-  it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
+  it(`should have as title 'app works!'`, () => {
+    expect(component.title).toEqual('app works!');
+  });
 
-  it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
+  it('should render title in a h1 tag', () => {
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+  });
 });
