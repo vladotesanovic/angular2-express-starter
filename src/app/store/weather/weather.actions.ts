ad4b9ef2d2b33a4c5db92095b7229e0f192ea988
Vlado Tešanović <vlado.tesanovic@gmail.com>
2017-07-24 22:43:27 +0200

    Update to ngrx 4.x and angular-cli

--- src/app/store/weather/weather.actions.ts
+++ src/app/store/weather/weather.actions.ts
@@ -1,18 +1,18 @@
 import { Action } from '@ngrx/store';
 
 export const WEATHER_GET = '[Weather] get weather for selected city';
-export const WEATHER_GET_SUCCESS = 'Weather: weather successfully get';
-export const WEATHER_GET_FAIL = 'Weather: error while fetching data';
+export const WEATHER_GET_SUCCESS = '[Weather] weather successfully get';
+export const WEATHER_GET_FAIL = '[Weather] error while fetching data';
 
-export const AIR_QUALITY_GET = 'Weather: get air quality';
-export const AIR_QUALITY_GET_SUCCESS = 'Weather: air quality successfully get';
-export const AIR_QUALITY_GET_FAIL = 'Weather: error while fetching air quality';
+export const AIR_QUALITY_GET = '[Weather] get air quality';
+export const AIR_QUALITY_GET_SUCCESS = '[Weather] air quality successfully get';
+export const AIR_QUALITY_GET_FAIL = '[Weather] error while fetching air quality';
 
-export const SELECT_CITY = 'Weather: Select city';
+export const SELECT_CITY = '[Weather] Select city';
 
-export const WEATHER_DATA_GET = 'Weather: get weather data';
-export const WEATHER_DATA_GET_SUCCESS = 'Weather: weather data successfully get';
-export const WEATHER_DATA_GET_FAIL = 'Weather: error while fetching wearher data';
+export const WEATHER_DATA_GET = '[Weather] get weather data';
+export const WEATHER_DATA_GET_SUCCESS = '[Weather] weather data successfully get';
+export const WEATHER_DATA_GET_FAIL = '[Weather] error while fetching wearher data';
 
 interface ILocation {
   longitude: number;
